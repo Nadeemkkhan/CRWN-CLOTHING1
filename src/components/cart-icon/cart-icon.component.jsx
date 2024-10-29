f6d9ab44bf3e8b4bd043de2e581319c175ae7869
NadeemKhan <zahidkhancoc786@gmail.com>
2024-10-29 18:05:19 +0530

    addded component

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action.js'
+
+
+import {ShoppingIcon, CartIconContainer, ItemCount} from './cart-icon.styles'
+
+
+const CartIcon = () => {
+
+const dispatch = useDispatch();
+
+const cartCount = useSelector(selectCartCount);
+const isCartOpen = useSelector(selectIsCartOpen);
+
+ const toggleIsCardOpen = () =>dispatch( setIsCartOpen(!isCartOpen));
+
+return (
+    <CartIconContainer onClick={toggleIsCardOpen}>
+        <ShoppingIcon  className='shopping-icon'/>
+        <ItemCount>{cartCount}</ItemCount>
+       </CartIconContainer>
+)
+}
+
+
+export default CartIcon;
